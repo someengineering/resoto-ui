0976e96e494d4c46dc3930ce6d4d5281d9693261
azagaya <azagaya.games@gmail.com>
2022-06-10 15:39:33 -0300

    Fix bugs when dashboard was not on the origin

--- src/components/widgets/Dashboard.gd
+++ src/components/widgets/Dashboard.gd
@@ -3,36 +3,58 @@ extends Control
 onready var widgets : Control = $Widgets
 onready var grid_background : ColorRect = $Grid
 
-export var x_grid_ratio := 10.0
-export var y_grid_size := 100.0
+export var x_grid_ratio := 10.0 
+export var y_grid_size := 100.0 
 export var grid_margin := Vector2(5,5)
 
 onready var _x_grid_size := rect_size.x / x_grid_ratio
+onready var widget_resizer_scene := preload("res://components/widgets/WidgetResizer.tscn")
 
-func _ready():
-	grid_background.rect_size = rect_size
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		add_widget()
+
+func add_widget():
+	var grid_size : Vector2 = Vector2(_x_grid_size, y_grid_size)
+	var widget = ColorRect.new()
+	widget.rect_size = Vector2(256,256)
+	widgets.add_child(widget)
+	var resizer = widget_resizer_scene.instance()
+	resizer.name = "WidgetResizer"
+	resizer.dashboard = self
+	widget.call_deferred("add_child", resizer)
+	resizer.grid_size = grid_size
+	widget.rect_position = widget.rect_position.snapped(grid_size) + grid_margin
+	widget.rect_size = widget.rect_size.snapped(grid_size) - 2*grid_margin
+	var reference = ReferenceRect.new()
+	resizer.parent_reference = reference
+	$References.add_child(reference)
 	
-	var grid_size := Vector2(_x_grid_size, y_grid_size)
-	for widget in $Widgets.get_children():
-		var widget_resizer = widget.get_node("WidgetResizer")
-		widget.rect_position = widget.rect_position.snapped(grid_size) + grid_margin
-		widget.rect_size = widget.rect_size.snapped(grid_size) - 2*grid_margin
-
-		widget_resizer.parent_reference.rect_global_position = widget_resizer.rect_global_position
-		widget_resizer.parent_reference.rect_size = widget_resizer.rect_size
-
-		widget_resizer.set_anchors()
+	yield(VisualServer,"frame_post_draw")
+	
+	reference.rect_global_position = resizer.rect_global_position
+	reference.rect_size = resizer.rect_size
+	
+	reference.mouse_filter = MOUSE_FILTER_IGNORE
+	reference.border_color = Color(0.662745, 0.568627, 0.792157)
+	reference.editor_only = false
+	reference.visible = false
+	
+	resizer.set_anchors()
+	
 		
 func _on_Grid_resized() -> void:
 	_x_grid_size = rect_size.x / x_grid_ratio
 
-	$Grid.material.set_shader_param("grid_x_ratio", x_grid_ratio)
+	var grid_size := Vector2(_x_grid_size, y_grid_size)
+	
+	$Grid.material.set_shader_param("grid_size", grid_size)
+	$Grid.material.set_shader_param("dashboard_size", rect_size)
 	for widget in $Widgets.get_children():
 		var widget_resizer = widget.get_node("WidgetResizer")
-		widget_resizer.dashboard = self
 		widget_resizer.grid_size.x = _x_grid_size
 		
-	yield(get_tree(),"idle_frame")
+	yield(VisualServer, "frame_post_draw")
 	for widget in $Widgets.get_children():
 		var widget_resizer = widget.get_node("WidgetResizer")
 		widget_resizer.parent_reference.set_deferred("rect_global_position" , widget_resizer.rect_global_position)
--- src/components/widgets/WidgetResizer.gd
+++ src/components/widgets/WidgetResizer.gd
@@ -1,4 +1,3 @@
-tool
 extends Control
 
 enum RESIZE_MODES {
@@ -22,9 +21,10 @@ var press_offset : Vector2
 var limits : Dictionary = {}
 var dashboard : Control
 
+var check_rect : Rect2
 
 onready var parent : Control = get_parent()
-onready var parent_reference : ReferenceRect = ReferenceRect.new()
+onready var parent_reference : ReferenceRect
 onready var resize_buttons := $ResizeButtons
 onready var resize_tween := $ResizeTween
 onready var last_good_position : Vector2
@@ -32,24 +32,12 @@ onready var last_good_size : Vector2
 
 
 func _ready() -> void:
-	call_deferred("add_child", parent_reference)
-	parent_reference.mouse_filter = MOUSE_FILTER_IGNORE
-	parent_reference.set_as_toplevel(true)
-	parent_reference.border_color = Color(0.662745, 0.568627, 0.792157)
-	parent_reference.editor_only = false
-	parent_reference.visible = false
 	for i in resize_buttons.get_child_count():
 		var button : BaseButton = resize_buttons.get_child(i)
 		button.connect("button_up", self, "_on_resize_button_released")
 		button.connect("button_down", self, "_on_resize_button_pressed", [i])
 	
 	set_process(false)
-	
-
-func _get_configuration_warning() -> String:
-	if not parent is Control:
-		return "Widget Resizer needs a Control node as parent."
-	return ""
 
 
 func _on_resize_button_released() -> void:
@@ -69,7 +57,7 @@ func _on_resize_button_released() -> void:
 
 func set_anchors():
 	var dashboard_size = dashboard.rect_size
-	parent.anchor_left = parent_reference.rect_position.x / dashboard_size.x
+	parent.anchor_left = (parent_reference.rect_position.x) / dashboard_size.x
 	parent.anchor_right = (parent_reference.rect_position + parent_reference.rect_size).x / dashboard_size.x
 	parent.margin_left = grid_margin.x
 	parent.margin_right = -grid_margin.x
@@ -80,8 +68,9 @@ func _on_resize_button_pressed( mode : int) -> void:
 	parent_reference.visible = true
 	parent_reference.rect_global_position = rect_global_position
 	parent_reference.rect_size = rect_size
-	last_pressed_position = get_global_mouse_position()
-	last_good_position = parent.rect_position
+	check_rect = parent_reference.get_rect()
+	last_pressed_position = dashboard.get_local_mouse_position().snapped(grid_size)
+	last_good_position = parent.rect_global_position
 	last_good_size = parent.rect_size
 	resize_mode = mode
 	press_offset = last_pressed_position - parent.rect_position
@@ -90,137 +79,136 @@ func _on_resize_button_pressed( mode : int) -> void:
 
 func _process(_delta : float) -> void:
 	var global_mouse_position = get_global_mouse_position()
-	var new_position : Vector2 = global_mouse_position.snapped(grid_size) 
+	var local_mouse_position = dashboard.get_local_mouse_position()
+	var new_position : Vector2 = local_mouse_position.snapped(grid_size) 
 	
 	match resize_mode:
 		RESIZE_MODES.TOP_LEFT:
 			var right_bot_corner = parent_reference.rect_position + parent_reference.rect_size - grid_size
 			var prev_rect_position : Vector2 = parent.rect_position
-			parent.rect_position.x = clamp(global_mouse_position.x, limits.left, right_bot_corner.x)
-			parent.rect_position.y = clamp(global_mouse_position.y, limits.top, right_bot_corner.y)
+			parent.rect_position.x = clamp(local_mouse_position.x, limits.left, right_bot_corner.x)
+			parent.rect_position.y = clamp(local_mouse_position.y, limits.top, right_bot_corner.y)
 			parent.rect_size += prev_rect_position - parent.rect_position
 			
 			new_position.x = clamp(new_position.x, limits.left, right_bot_corner.x)
 			new_position.y = clamp(new_position.y, limits.top, right_bot_corner.y)
 			
 			if new_position != last_pressed_position:
-				resize_tween.interpolate_property(parent_reference, "rect_position", parent_reference.rect_position, new_position, 0.2)
-				resize_tween.interpolate_property(parent_reference, "rect_size", parent_reference.rect_size, parent_reference.rect_size - (new_position - parent_reference.rect_position), 0.2)
-				resize_tween.start()
+				check_rect.position = new_position
+				check_rect.size = parent_reference.rect_size - (new_position - parent_reference.rect_position)
+				
 
 		RESIZE_MODES.BOTTOM_RIGHT:
 			var max_right_corner = parent_reference.rect_position + grid_size
 			
-			parent.rect_size.x = clamp(global_mouse_position.x, max_right_corner.x, limits.right) - parent.rect_position.x
-			parent.rect_size.y = clamp(global_mouse_position.y, max_right_corner.y, limits.bottom) - parent.rect_position.y
+			parent.rect_size.x = clamp(local_mouse_position.x, max_right_corner.x, limits.right) - parent.rect_position.x
+			parent.rect_size.y = clamp(local_mouse_position.y, max_right_corner.y, limits.bottom) - parent.rect_position.y
 			
 			new_position.x = clamp(new_position.x, max_right_corner.x, limits.right)
 			new_position.y = clamp(new_position.y, max_right_corner.y, limits.bottom)
 			
 			if new_position != last_pressed_position:
-				resize_tween.interpolate_property(parent_reference, "rect_size", parent_reference.rect_size, new_position - parent_reference.rect_position, 0.2)
-				resize_tween.start()
+				check_rect.size = new_position - check_rect.position
+		
 			
 		RESIZE_MODES.TOP_RIGHT:
 			var min_right = parent_reference.rect_position.x + grid_size.x
 			var max_top = parent_reference.rect_position.y + parent_reference.rect_size.y  - grid_size.y
 			var prev_rect_position : Vector2 = parent.rect_position
 			
-			parent.rect_position.y = clamp(global_mouse_position.y, limits.top, max_top)
-			parent.rect_size.x = clamp(global_mouse_position.x, min_right, limits.right) - parent.rect_position.x
+			parent.rect_position.y = clamp(local_mouse_position.y, limits.top, max_top)
+			parent.rect_size.x = clamp(local_mouse_position.x, min_right, limits.right) - parent.rect_position.x
 			parent.rect_size.y += prev_rect_position.y - parent.rect_position.y
 			
 			new_position.x = clamp(new_position.x, min_right, limits.right)
 			new_position.y = clamp(new_position.y, limits.top, max_top)
 			
 			if new_position != last_pressed_position:
-				resize_tween.interpolate_property(parent_reference, "rect_position:y", parent_reference.rect_position.y, new_position.y, 0.2)
-				resize_tween.interpolate_property(parent_reference, "rect_size:y", parent_reference.rect_size.y, parent_reference.rect_size.y - (new_position.y - parent_reference.rect_position.y), 0.2)
-				resize_tween.interpolate_property(parent_reference, "rect_size:x", parent_reference.rect_size.x, new_position.x - parent_reference.rect_position.x, 0.2)
-				resize_tween.start()
+				check_rect.position.y = new_position.y
+				check_rect.size = Vector2(new_position.x - parent_reference.rect_position.x, parent_reference.rect_size.y - (new_position.y - parent_reference.rect_position.y))
+
 				
 		RESIZE_MODES.BOTTOM_LEFT:
 			var max_left = parent_reference.rect_position.x + parent_reference.rect_size.x - grid_size.x 
 			var min_bottom = parent_reference.rect_position.y + grid_size.y
 			var prev_rect_position : Vector2 = parent.rect_position
 			
-			parent.rect_position.x = clamp(global_mouse_position.x, limits.left, max_left)
+			parent.rect_position.x = clamp(local_mouse_position.x, limits.left, max_left)
 			parent.rect_size.x += prev_rect_position.x - parent.rect_position.x
-			parent.rect_size.y = clamp(global_mouse_position.y, min_bottom, limits.bottom) - parent.rect_position.y
+			parent.rect_size.y = clamp(local_mouse_position.y, min_bottom, limits.bottom) - parent.rect_position.y
 			
 			new_position.x = clamp(new_position.x, limits.left, max_left)
 			new_position.y = clamp(new_position.y, min_bottom, limits.bottom)
 			
 			if new_position != last_pressed_position:
-				resize_tween.interpolate_property(parent_reference, "rect_position:x", parent_reference.rect_position.x, new_position.x, 0.2)
-				resize_tween.interpolate_property(parent_reference, "rect_size:x", parent_reference.rect_size.x, parent_reference.rect_size.x - (new_position.x - parent_reference.rect_position.x), 0.2)
-				resize_tween.interpolate_property(parent_reference, "rect_size:y", parent_reference.rect_size.y, new_position.y - parent_reference.rect_position.y, 0.2)
-				resize_tween.start()
+				check_rect.position.x = new_position.x
+				check_rect.size = Vector2(parent_reference.rect_size.x - (new_position.x - parent_reference.rect_position.x), new_position.y - parent_reference.rect_position.y)
 				
 		RESIZE_MODES.BOTTOM_CENTER:
 			var min_bottom = parent_reference.rect_position.y  + grid_size.y
 			
-			parent.rect_size.y = clamp(global_mouse_position.y, min_bottom, limits.bottom) - parent.rect_position.y
+			parent.rect_size.y = clamp(local_mouse_position.y, min_bottom, limits.bottom) - parent.rect_position.y
 			new_position.y = clamp(new_position.y, min_bottom, limits.bottom)
 			
 			if new_position != last_pressed_position:
-				resize_tween.interpolate_property(parent_reference, "rect_size:y", parent_reference.rect_size.y, new_position.y - parent_reference.rect_position.y, 0.2)
-				resize_tween.start()
+				check_rect.size.y = new_position.y - parent_reference.rect_position.y
 				
 		RESIZE_MODES.TOP_CENTER:
 			var max_top = parent_reference.rect_position.y + parent_reference.rect_size.y - grid_size.y 
 			var prev_rect_position = parent.rect_position
 			
-			parent.rect_position.y = clamp(global_mouse_position.y, limits.top, max_top)
+			parent.rect_position.y = clamp(local_mouse_position.y, limits.top, max_top)
 			parent.rect_size.y += prev_rect_position.y - parent.rect_position.y
 			new_position.y = clamp(new_position.y, limits.top, max_top)
 			
 			if new_position.y != last_pressed_position.y :
-				resize_tween.interpolate_property(parent_reference, "rect_position:y", parent_reference.rect_position.y, new_position.y, 0.2)
-				resize_tween.interpolate_property(parent_reference, "rect_size:y", parent_reference.rect_size.y, parent_reference.rect_size.y - (new_position - parent_reference.rect_position).y, 0.2)
-				resize_tween.start()
+				check_rect.position.y = new_position.y
+				check_rect.size.y = parent_reference.rect_size.y - (new_position - parent_reference.rect_position).y
 				
 		RESIZE_MODES.MID_LEFT:
 			var max_left = parent_reference.rect_position.x + parent_reference.rect_size.x - grid_size.x 
 			var prev_rect_position = parent.rect_position
-			parent.rect_position.x = clamp(global_mouse_position.x, limits.left, max_left)
+			parent.rect_position.x = clamp(local_mouse_position.x, limits.left, max_left)
 			parent.rect_size.x += prev_rect_position.x - parent.rect_position.x
 			new_position.x = clamp(new_position.x, limits.left, max_left)
 			
 			if new_position.x != last_pressed_position.x:
-				resize_tween.interpolate_property(parent_reference, "rect_position:x", parent_reference.rect_position.x, new_position.x, 0.2)
-				resize_tween.interpolate_property(parent_reference, "rect_size:x", parent_reference.rect_size.x, parent_reference.rect_size.x - (new_position - parent_reference.rect_position).x, 0.2)
-				resize_tween.start()
+				check_rect.position.x = new_position.x
+				check_rect.size.x = parent_reference.rect_size.x - (new_position - parent_reference.rect_position).x
 			
 		RESIZE_MODES.MID_RIGHT:
 			var min_right = parent_reference.rect_position.x + grid_size.x 
 			new_position.x = clamp(new_position.x, min_right, limits.right)
-			parent.rect_size.x = clamp(global_mouse_position.x, min_right, limits.right) - parent.rect_position.x
+			parent.rect_size.x = clamp(local_mouse_position.x, min_right, limits.right) - parent.rect_position.x
 			
 			if new_position.x != last_pressed_position.x:
-				resize_tween.interpolate_property(parent_reference, "rect_size:x", parent_reference.rect_size.x, new_position.x - parent_reference.rect_position.x, 0.2)
-				resize_tween.start()
-				
+				check_rect.size.x = new_position.x - parent_reference.rect_position.x
 				
 		RESIZE_MODES.MOVE:
-			new_position = (global_mouse_position-press_offset).snapped(grid_size) 
+			new_position = (local_mouse_position-press_offset).snapped(grid_size) 
 			
-			var test_rect = parent_reference.get_global_rect()
+			var test_rect = parent_reference.get_rect()
 			test_rect.position = new_position
 			test_rect = test_rect.grow_individual(-1,-1,-1,-1)
 			test_rect = find_rect_intersection(test_rect)
-			parent.rect_position = global_mouse_position - press_offset
+			parent.rect_position = local_mouse_position - press_offset
 			
 			if test_rect != null:
 				return
-			
-			
+			6
 			if new_position != last_pressed_position:
-				resize_tween.interpolate_property(parent_reference, "rect_position", parent_reference.rect_position, new_position, 0.2)
-				resize_tween.start()
+				check_rect.position = new_position
 			
-			
+	if last_pressed_position != new_position:
+		animate_reference_rect()
+		limits = get_limits()
+		
 	last_pressed_position = new_position
+	
+func animate_reference_rect():
+	resize_tween.interpolate_property(parent_reference, "rect_position", parent_reference.rect_position, check_rect.position, 0.2)
+	resize_tween.interpolate_property(parent_reference, "rect_size", parent_reference.rect_size, check_rect.size, 0.2)
+	resize_tween.start()
 			
 func get_limits() -> Dictionary:
 	var limits := {
@@ -253,7 +241,7 @@ func segment_overlap(a_min,a_max,b_min,b_max):
 		( a_max <= b_max and a_max >= b_min ) or (a_min <= b_min and a_max >= b_max) )
 	
 func find_next_rect(direction : Vector2, N=10):
-	var rect : Rect2 = parent_reference.get_global_rect()
+	var rect : Rect2 = check_rect
 	direction *= grid_size
 	for i in N:
 		rect = rect.grow_individual(-direction.x if direction.x < 0 else 0,
@@ -271,7 +259,8 @@ func find_rect_intersection(rect : Rect2 = parent_reference.get_global_rect()):
 	for widget in all_widgets:
 		if widget == parent:
 			continue
-		var other_rect : Rect2 = widget.get_node("WidgetResizer").parent_reference.get_global_rect()
+		var other_rect : Rect2 = widget.get_node("WidgetResizer").parent_reference.get_rect()
+
 		if other_rect.intersects(rect):
 			return other_rect
 	return null
