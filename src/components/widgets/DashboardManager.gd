8822059e4d9ea41e92b289724b14c3e070b820b1
azagaya <azagaya.games@gmail.com>
2022-06-21 10:32:42 -0300

    Start with dashboard manager view

---
+++ src/components/widgets/DashboardManager.gd
@@ -0,0 +1,10 @@
+extends TabContainer
+
+var dashboard_container_scene := preload("res://components/widgets/DashboardContainer.tscn")
+
+func _on_DashBoardManager_tab_selected(tab):
+	if get_tab_title(tab) == "+":
+		var new_tab = dashboard_container_scene.instance()
+		add_child(new_tab,true)
+		move_child(new_tab, get_tab_control(tab).get_position_in_parent())
+
