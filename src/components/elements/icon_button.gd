fde83a36a7825392b5834d3c7bc7d4f19a52f1d1
Raffaele Picca <picster@pixelgod.net>
2022-10-11 16:37:23 +0200

    Added a basic, styled Icon Button

--- src/components/elements/icon_button.gd
+++ src/components/elements/icon_button.gd
@@ -1,7 +1,35 @@
 extends Button
 
-export (Texture) var icon_tex
+export (Texture) var icon_tex setget set_icon_tex
+
+var flip_h:bool = false setget set_flip_h
+var flip_v:bool = false setget set_flip_v
+
+onready var icon_node = $Icon
+
 
 func _ready():
-	$Icon.texture = icon_tex
-	_t.style($Icon, _t.col.C_LIGHT)
+	icon_node.texture = icon_tex
+	_t.style(icon_node, _t.col.C_LIGHT)
+
+
+func set_icon_tex(_icon_tex:Texture):
+	icon_tex = _icon_tex
+	if icon_node:
+		icon_node.texture = icon_tex
+
+
+func set_flip_h(_flip_h:bool):
+	icon_node.flip_h = _flip_h
+
+
+func set_flip_v(_flip_v:bool):
+	icon_node.flip_v = _flip_v
+
+
+func _on_IconButton_mouse_entered():
+	icon_node.modulate = _t.col_map[_t.col.C_LIGHTER]
+
+
+func _on_IconButton_mouse_exited():
+	icon_node.modulate = _t.col_map[_t.col.C_LIGHT]
--- src/scripts/autoloads/theme_manager.gd
+++ src/scripts/autoloads/theme_manager.gd
@@ -8,22 +8,22 @@ const themed_group_name:= "themed"
 const themed_col_name:= "theme_color"
 enum col {C_NORMAL, C_LIGHT, C_LIGHTER, C_DARK, C_DARKER, C_BG, C_BG2, C_BG_BACK}
 
-var color_map:= {
-	col.C_NORMAL : { 	"dark" : Color("#0054a3"), "light" : Color("#0054a3") },
-	col.C_LIGHT : { 	"dark" : Color("#89d1f1"), "light" : Color("#89d1f1") },
-	col.C_LIGHTER : { 	"dark" : Color("#ffffff"), "light" : Color("#ffffff") },
-	col.C_DARK : { 		"dark" : Color("#0054a3"), "light" : Color("#0054a3") },
-	col.C_DARKER : { 	"dark" : Color("#0f3356"), "light" : Color("#0f3356") },
-	col.C_BG : { 		"dark" : Color("#0a253f"), "light" : Color("#0a253f") },
-	col.C_BG2 : { 		"dark" : Color("#0c1822"), "light" : Color("#0c1822") },
-	col.C_BG_BACK : {	"dark" : Color("#171d21"), "light" : Color("#171d21") }
+var col_map:= {
+	col.C_NORMAL:	Color("#0054a3"),
+	col.C_LIGHT:	Color("#89d1f1"),
+	col.C_LIGHTER:	Color("#ffffff"),
+	col.C_DARK: 	Color("#0054a3"),
+	col.C_DARKER:	Color("#0f3356"),
+	col.C_BG:		Color("#0a253f"),
+	col.C_BG2:		Color("#0c1822"),
+	col.C_BG_BACK:	Color("#171d21")
 }
 
 func update_theme():
 	var nodes_to_style = get_tree().get_nodes_in_group(themed_group_name)
 	for i in nodes_to_style:
 		var orig_alpha = i.modulate.a
-		i.modulate = color_map[i.get_meta(themed_col_name)].dark
+		i.modulate = col_map[i.get_meta(themed_col_name)]
 		i.modulate.a = orig_alpha
 
 
@@ -31,5 +31,5 @@ func style(node:Node, color_id:int):
 	node.add_to_group(themed_group_name)
 	node.set_meta(themed_col_name, color_id)
 	var orig_alpha = node.modulate.a
-	node.modulate = color_map[color_id].dark
+	node.modulate = col_map[color_id]
 	node.modulate.a = orig_alpha
