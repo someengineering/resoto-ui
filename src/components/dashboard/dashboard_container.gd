09f495aed3b361ae5ded8e4cbfc23f36d62433da
azagaya <azagaya.games@gmail.com>
2022-10-19 14:25:58 -0300

    Remove duplicated date button

--- src/components/dashboard/dashboard_container.gd
+++ src/components/dashboard/dashboard_container.gd
@@ -34,7 +34,7 @@ var is_maximized: bool = false setget set_is_maximized
 
 var initial_load : bool = true
 
-onready var date_button := $VBoxContainer/PanelContainer/DateButton
+onready var date_button := $VBoxContainer/PanelContainer/Content/HFlowContainer/DateButton
 onready var dashboard := $VBoxContainer/ScrollContainer/Content/Dashboard
 onready var add_widget_popup := $NewWidgetPopup
 onready var range_selector := $DateRangeSelector
