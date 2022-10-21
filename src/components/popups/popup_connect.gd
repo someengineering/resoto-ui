f399251ea329d53d1d06932ae9ffa6a6fa4f3c00
Raffaele Picca <picster@pixelgod.net>
2022-10-21 08:52:17 +0200

    Small fix to not use -1 as the port but to keep the default port.

--- src/components/popups/popup_connect.gd
+++ src/components/popups/popup_connect.gd
@@ -44,8 +44,7 @@ func connect_to_core() -> void:
 		var port : String = JavaScript.eval("getPort()")
 		if port != "":
 			API.port = int(port)
-		else:
-			API.port = -1
+	
 	
 	status.text = CONNECT_TEXT.format(["0", API.adress, API.port])
 	yield(VisualServer, "frame_post_draw")
