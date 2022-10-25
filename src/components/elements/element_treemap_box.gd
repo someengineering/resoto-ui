3bc58ff1abac5ee64f7279a249eccc4041addd59
Raffaele Picca <picster@pixelgod.net>
2022-10-25 16:21:31 +0200

    Improved Treemap charts; labels size dynamically; label color changes when background is dark

--- src/components/elements/element_treemap_box.gd
+++ src/components/elements/element_treemap_box.gd
@@ -8,24 +8,40 @@ var value := 0.0
 var final_size := Vector2.ZERO
 
 onready var text_label = $Center/C/Z/Label
+onready var scaler = $Center/C/Z
 
 
 func _ready():
 	text_label.text = label + "\n(" + str(value) + ")"
 	$Button.hint_tooltip = label + ": " + str(value)
 	$Button.modulate = element_color
+	var font_color = Style.col_map[Style.c.BG2] if element_color.get_luminance() > 0.46 else Style.col_map[Style.c.LIGHT]
+	text_label.add_color_override("font_color", font_color)
 	yield(VisualServer, "frame_post_draw")
 	update_label_pos()
 
 
 func update_label_pos():
-	if final_size.x < 100 or final_size.y < 80:
+	text_label.show()
+	if final_size.x < 30 or final_size.y < 30:
 		text_label.hide()
+	elif final_size.x < 60 or final_size.y < 40:
+		text_label.rect_size.x = final_size.x - 2
+		text_label.rect_size.y = final_size.y - 2
+		text_label.rect_size /= 0.5
+		text_label.rect_position = -text_label.rect_size / 2
+		scaler.scale = Vector2.ONE*0.5
+	elif final_size.x < 150 or final_size.y < 120:
+		text_label.rect_size.x = final_size.x - 7
+		text_label.rect_size.y = final_size.y - 7
+		text_label.rect_size /= 0.7
+		text_label.rect_position = -text_label.rect_size / 2
+		scaler.scale = Vector2.ONE*0.7
 	else:
-		text_label.show()
 		text_label.rect_size.x = final_size.x - 10
 		text_label.rect_size.y = final_size.y - 10
 		text_label.rect_position = -text_label.rect_size / 2
+		scaler.scale = Vector2.ONE
 
 
 func _on_Button_mouse_entered():
