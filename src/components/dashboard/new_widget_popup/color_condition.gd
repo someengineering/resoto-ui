aec0f9cdafda6711ca1b86fef32aa97e2fa0bd1b
Raffaele Picca <picster@pixelgod.net>
2022-10-25 13:28:19 +0200

    Changed the old Color Picker in the Widget Edit dialogue

--- src/components/dashboard/new_widget_popup/color_condition.gd
+++ src/components/dashboard/new_widget_popup/color_condition.gd
@@ -3,6 +3,8 @@ extends MarginContainer
 
 signal condition_changed()
 
+const var_text = "at %s"
+
 var variable_name : String = "" setget set_variable_name
 var value : float = 0.0 setget set_value
 var color : Color = Color.black setget set_color
@@ -20,18 +22,19 @@ func set_value(new_value : float):
 
 func set_color(new_color : Color):
 	color = new_color
-	$PanelContainer/HBoxContainer/ColorPickerButton.color = color
+	condition[1] = color
+	$PanelContainer/HBoxContainer/HexColorPicker.color = color
 
 func _on_SpinBox_value_changed(new_value):
 	condition[0] = new_value
 	emit_signal("condition_changed")
 
 
-func _on_ColorPickerButton_color_changed(new_color):
+func set_variable_name(new_name : String):
+	variable_name = var_text % new_name
+	$PanelContainer/HBoxContainer/VariableLabel.text = variable_name
+
+
+func _on_HexColorPicker_color_changed(new_color):
 	condition[1] = new_color
 	emit_signal("condition_changed")
-
-
-func set_variable_name(new_name : String):
-	variable_name = new_name
-	$PanelContainer/HBoxContainer/VariableLabel.text = variable_name
