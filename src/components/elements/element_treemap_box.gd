4082f00d2414cce93c3d9ea6fa0abfadc5998333
Raffaele Picca <picster@pixelgod.net>
2022-10-26 09:59:32 +0200

    Typing and small optimizations

--- src/components/elements/element_treemap_box.gd
+++ src/components/elements/element_treemap_box.gd
@@ -15,7 +15,7 @@ func _ready():
 	text_label.text = label + "\n(" + str(value) + ")"
 	$Button.hint_tooltip = label + ": " + str(value)
 	$Button.modulate = element_color
-	var font_color = Style.col_map[Style.c.BG2] if element_color.get_luminance() > 0.46 else Style.col_map[Style.c.LIGHT]
+	var font_color: Color = Style.col_map[Style.c.BG2] if element_color.get_luminance() > 0.46 else Style.col_map[Style.c.LIGHT]
 	text_label.add_color_override("font_color", font_color)
 	yield(VisualServer, "frame_post_draw")
 	update_label_pos()
@@ -26,20 +26,15 @@ func update_label_pos():
 	if final_size.x < 30 or final_size.y < 30:
 		text_label.hide()
 	elif final_size.x < 60 or final_size.y < 40:
-		text_label.rect_size.x = final_size.x - 2
-		text_label.rect_size.y = final_size.y - 2
-		text_label.rect_size /= 0.5
+		text_label.rect_size = (final_size - Vector2(2,2)) * 2.0 # / 0.5
 		text_label.rect_position = -text_label.rect_size / 2
-		scaler.scale = Vector2.ONE*0.5
+		scaler.scale = Vector2.ONE * 0.5
 	elif final_size.x < 150 or final_size.y < 120:
-		text_label.rect_size.x = final_size.x - 7
-		text_label.rect_size.y = final_size.y - 7
-		text_label.rect_size /= 0.7
+		text_label.rect_size = (final_size - Vector2(7,7)) / 0.7
 		text_label.rect_position = -text_label.rect_size / 2
-		scaler.scale = Vector2.ONE*0.7
+		scaler.scale = Vector2.ONE * 0.7
 	else:
-		text_label.rect_size.x = final_size.x - 10
-		text_label.rect_size.y = final_size.y - 10
+		text_label.rect_size = final_size - Vector2(10,10)
 		text_label.rect_position = -text_label.rect_size / 2
 		scaler.scale = Vector2.ONE
 
