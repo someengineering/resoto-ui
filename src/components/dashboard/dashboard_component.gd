51e55885852f7c5d9347a028ece8253c78204953
azagaya <46932830+azagaya@users.noreply.github.com>
2022-11-22 10:04:55 -0300

    Change title name after updating the view

--- src/components/dashboard/dashboard_component.gd
+++ src/components/dashboard/dashboard_component.gd
@@ -2,9 +2,9 @@ extends ComponentContainer
 
 func apply_navigation_arguments():
 	if "dashboard_name" in navigation_arguments:
-		UINavigation.change_title("Dashboard: %s" % navigation_arguments["dashboard_name"])
 		component = component as DashboardManager
 		component.load_dashboard(navigation_arguments["dashboard_name"])
+		UINavigation.change_title("Dashboard: %s" % navigation_arguments["dashboard_name"])
 
 
 func _on_DashBoardManager_dashboard_opened(dashboard_name):
