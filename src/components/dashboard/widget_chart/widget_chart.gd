c35eb243d6de965006a37657e3371733a68cc2b3
azagaya <46932830+azagaya@users.noreply.github.com>
2022-11-22 10:48:50 -0300

    Some fixes to Line Chart (#116)
    * Try to fix charts not displaying y labels
    
    * Fix offset in y labels when min value is not zero

--- src/components/dashboard/widget_chart/widget_chart.gd
+++ src/components/dashboard/widget_chart/widget_chart.gd
@@ -222,7 +222,7 @@ func update_graph_area(force := false) -> void:
 			l.text = str(value)
 			l.align = Label.ALIGN_RIGHT
 			l.valign = Label.VALIGN_CENTER
-			l.rect_position.y = round(y_labels.rect_size.y * (y_zero_position - i/ny)) - l.rect_size.y / 2
+			l.rect_position.y = round(y_labels.rect_size.y * (zero - i/ny)) - l.rect_size.y / 2
 			l.anchor_right = 1
 			l.margin_right = 0
 			y_labels.add_child(l)
@@ -283,13 +283,14 @@ func set_scale_from_series() -> void:
 				maxy = value.y
 	
 	
-	max_y_value = round(maxy + (maxy - miny) * 0.1 if maxy != miny else maxy * 1.1)
-	min_y_value = round(miny - (maxy - miny) * 0.1 if maxy != miny else miny * 0.9)
+	max_y_value = ceil(maxy + (maxy - miny) * 0.1 if maxy != miny else maxy * 1.1)
+	min_y_value = floor(miny - (maxy - miny) * 0.1 if maxy != miny else miny * 0.9)
 	if miny == 0 or maxy == miny:
 		min_y_value = 0
 	
-	y_zero_position = max_y_value / (max_y_value - min_y_value)
-	grid.material.set_shader_param("zero_position", y_zero_position)
+	if max_y_value != min_y_value:
+		y_zero_position = max_y_value / (max_y_value - min_y_value)
+		grid.material.set_shader_param("zero_position", y_zero_position)
 
 
 func do_complete_update():
