ecb92528740bb74cd9ef69324f0839561d0fccb9
Raffaele Picca <picster@pixelgod.net>
2022-11-28 16:46:44 +0100

    Fix collect run status when waiting for another workflow

--- src/components/wizard/wizard_steps/custom_steps/wizard_step_custom_collect_run_status.gd
+++ src/components/wizard/wizard_steps/custom_steps/wizard_step_custom_collect_run_status.gd
@@ -2,8 +2,9 @@ extends WizardStep
 
 func start(_data:Dictionary):
 	emit_signal("can_previous", false)
+	$VBoxContainer/CollectRunDisplay.wait_for_core()
+	yield(get_tree(), "idle_frame")
 	API.cli_execute("workflow run collect_and_cleanup", self)
-	$VBoxContainer/CenterContainer/Loading.start()
 
 
 func _on_cli_execute_done(error:int, _response:UserAgent.Response) -> void:
@@ -15,3 +16,7 @@ func _on_CollectRunDisplay_all_done():
 	$VBoxContainer/CenterContainer/Loading.stop()
 	Analytics.event(Analytics.EventWizard.COLLECT_DONE)
 	emit_signal("can_continue")
+
+
+func _on_CollectRunDisplay_started():
+	$VBoxContainer/CenterContainer/Loading.start()
