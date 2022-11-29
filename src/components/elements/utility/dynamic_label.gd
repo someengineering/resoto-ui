3c19510f68be5476e0422805ef029f00c9d10b98
azagaya <azagaya.games@gmail.com>
2022-11-29 09:29:16 -0300

    Fix Dynamic labels font size not resizing when needed

--- src/components/elements/utility/dynamic_label.gd
+++ src/components/elements/utility/dynamic_label.gd
@@ -13,7 +13,7 @@ var resizeing := false
 var prev_size := Vector2.ZERO
 
 func _on_DynamicLabel_resized() -> void:
-	if not resizeing and rect_size.distance_to(prev_size) > 10:
+	if not resizeing and rect_size.distance_to(prev_size) > 1:
 		resizeing = true
 		set_process(true)
 		prev_size = rect_size
@@ -24,6 +24,7 @@ func _process(_delta : float) -> void:
 		var size : Vector2
 		var string_size := font.get_string_size(text)
 		size = font.size * base_width_ratio * rect_size / string_size
+		size = Vector2(floor(size.x), floor(size.y))
 		if abs(min(size.x, size.y) - font.size) > 1:
 			font.size = int(clamp(min(size.x, size.y), min_font_size, max_font_size))
 	set_process(false)
