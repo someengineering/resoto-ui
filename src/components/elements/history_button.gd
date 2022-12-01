db042e31cb37dcc34234046af9d42a8419d6ebc0
Raffaele Picca <picster@pixelgod.net>
2022-12-01 17:09:00 +0100

    Added a Resh Lite popup that can be opened anywhere. (#170)

--- src/components/elements/history_button.gd
+++ src/components/elements/history_button.gd
@@ -1,6 +1,7 @@
 extends ToolButton
 tool
 
+export var px_offset := 8.0
 export var forward : bool = false setget set_forward
 export var can_press : bool = true setget set_can_press
 
@@ -9,13 +10,18 @@ onready var btn := $Button
 onready var tween := $Tween
 
 
-func _on_HistoryButton_pressed():
-	var target_pos = 8 if forward else -8
+func _ready():
+	connect("pressed", self, "_on_Button_pressed")
+
+
+func _on_Button_pressed():
+	var target_pos = px_offset if forward else -px_offset
 	tween.interpolate_property(icon_arrow, "rect_scale", Vector2(0.2, 0.6), Vector2.ONE, 0.3, Tween.TRANS_EXPO, Tween.EASE_OUT)
 	tween.interpolate_property(btn, "rect_position:x", btn.rect_position.x, target_pos, 0.1, Tween.TRANS_EXPO, Tween.EASE_OUT)
 	tween.interpolate_property(btn, "rect_position:x", target_pos, 0, 0.2, Tween.TRANS_EXPO, Tween.EASE_OUT, 0.1)
 	tween.interpolate_property(icon_arrow, "modulate", Color.white*2, Color.white, 0.3, Tween.TRANS_EXPO, Tween.EASE_OUT)
 	tween.start()
+	release_focus()
 
 
 func set_forward(_forward:bool) -> void:
