23edaa4190c3be8b10a37f5c0f0a285f06205928
Raffaele Picca <picster@pixelgod.net>
2022-12-02 11:51:00 +0100

    Improved zoom button group

---
+++ src/components/shared/zoom_elements.gd
@@ -0,0 +1,27 @@
+extends HBoxContainer
+
+
+onready var zoom_label = $LabelSpacer/Control/Attach/ZoomScaleLabel
+onready var zoom_spacer = $LabelSpacer
+onready var zoom_scaler = $LabelSpacer/Control/Attach
+
+
+func _ready():
+	get_tree().root.connect("size_changed", self, "on_ui_scale_changed")
+	_g.connect("ui_scale_changed", self, "on_ui_scale_changed")
+
+
+func _on_ButtonUIScaleMinus_pressed():
+	_g.emit_signal("ui_scale_decrease")
+
+
+func _on_ButtonUIScalePlus_pressed():
+	_g.emit_signal("ui_scale_increase")
+
+
+func on_ui_scale_changed():
+	zoom_label.text = str(_g.ui_scale*100) + "%"
+	zoom_spacer.rect_min_size.x = 40 / _g.ui_scale
+	zoom_scaler.scale = Vector2.ONE / _g.ui_scale
+	$ButtonUIScaleMinus.rect_min_size.x = 24 / _g.ui_scale
+	$ButtonUIScalePlus.rect_min_size.x = 24 / _g.ui_scale
