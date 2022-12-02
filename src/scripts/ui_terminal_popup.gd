66da684eb3f115b416e879a5daa935bb28302b74
Raffaele Picca <picster@pixelgod.net>
2022-12-02 13:46:09 +0100

    Fixing the y position offset on reopening Resh Lite (#173)

--- src/scripts/ui_terminal_popup.gd
+++ src/scripts/ui_terminal_popup.gd
@@ -28,14 +28,14 @@ func show_terminal_popup():
 	if terminal_popup_rect.size == Vector2(0,0):
 		terminal_popup.show()
 		tween.interpolate_property(terminal_popup, "modulate:a", terminal_popup.modulate.a, 1, 0.4, Tween.TRANS_EXPO, Tween.EASE_OUT)
-		tween.interpolate_property(terminal_popup, "rect_position", DEFAULT_POSITION+Vector2(0, 30), DEFAULT_POSITION, 0.4, Tween.TRANS_EXPO, Tween.EASE_OUT)
+		tween.interpolate_property(terminal_popup, "rect_position", DEFAULT_POSITION, DEFAULT_POSITION, 0.4, Tween.TRANS_EXPO, Tween.EASE_OUT)
 		tween.interpolate_property(terminal_popup, "rect_scale", Vector2(1, 0.4), Vector2.ONE, 0.1, Tween.TRANS_EXPO, Tween.EASE_OUT)
 		tween.start()
 	else:
 		terminal_popup.show()
 		terminal_popup.rect_pivot_offset = terminal_popup_rect.size/2
 		tween.interpolate_property(terminal_popup, "modulate:a", terminal_popup.modulate.a, 1, 0.4, Tween.TRANS_EXPO, Tween.EASE_OUT)
-		tween.interpolate_property(terminal_popup, "rect_position", terminal_popup_rect.position+Vector2(0, 30), terminal_popup_rect.position, 0.4, Tween.TRANS_EXPO, Tween.EASE_OUT)
+		tween.interpolate_property(terminal_popup, "rect_position", terminal_popup_rect.position, terminal_popup_rect.position, 0.4, Tween.TRANS_EXPO, Tween.EASE_OUT)
 		tween.interpolate_property(terminal_popup, "rect_scale", Vector2(1, 0.4), Vector2.ONE, 0.1, Tween.TRANS_EXPO, Tween.EASE_OUT)
 		tween.start()
 		terminal_popup.rect_size = terminal_popup_rect.size
