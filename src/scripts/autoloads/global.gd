33a4d4b4648bd061205649815b32201dd2ea8610
Raffaele Picca <picster@pixelgod.net>
2023-02-22 15:42:02 +0100

    Bump version to 3.2.3

--- src/scripts/autoloads/global.gd
+++ src/scripts/autoloads/global.gd
@@ -64,7 +64,7 @@ const discord_link:String			 = "https://discord.gg/someengineering"
 
 var ui_test_mode: bool = false
 var ui_scale: float = 1.0 setget set_ui_scale
-var ui_version:String = "3.2.2"
+var ui_version:String = "3.2.3"
 var resotocore_version:String = "n/a"
 var os = "Windows"
 var terminal_scrollback:Array = []
