fb989395293445adca0557e31a13403c6d5a9c90
Raffaele Picca <picster@pixelgod.net>
2023-03-01 16:10:00 +0100

    Fix problem when the Resh Lite popup and Resh Lite component is opened together

--- src/scripts/ui_terminal_manager.gd
+++ src/scripts/ui_terminal_manager.gd
@@ -2,6 +2,8 @@ extends VBoxContainer
 
 var TerminalScene: PackedScene = preload("res://components/terminal/component_terminal.tscn")
 
+var is_popup := false
+
 var terminals: Array = []
 var _active_tab_id: int = -1
 
@@ -38,6 +40,7 @@ func change_name(_new_name:String) -> void:
 func add_new_terminal_tab() -> void:
 	tabs.add_tab("Terminal " + str(tabs.get_tab_count()+1))
 	var new_terminal = TerminalScene.instance()
+	new_terminal.is_popup = is_popup
 	new_terminal.connect("rename_terminal", self, "on_rename_terminal", [new_terminal])
 	terminals.append(new_terminal)
 	container.add_child(new_terminal)
