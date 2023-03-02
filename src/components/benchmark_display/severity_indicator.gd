6ce97ccfd1eb5d662ca4fe0a63197ed8afba6158
Raffaele Picca <picster@pixelgod.net>
2023-03-02 14:56:18 +0100

    Changed the Design of the Benchmark views right window.

--- src/components/benchmark_display/severity_indicator.gd
+++ src/components/benchmark_display/severity_indicator.gd
@@ -1,20 +1,43 @@
-extends HBoxContainer
+extends Control
 
+const severity_textures := [
+	preload("res://assets/icons/icon-benchmark-severity-1.svg"),
+	preload("res://assets/icons/icon-benchmark-severity-2.svg"),
+	preload("res://assets/icons/icon-benchmark-severity-3.svg"),
+	preload("res://assets/icons/icon-benchmark-severity-4.svg")
+	]
+const severity_colors := [
+	"#89d1f1", "#fffd7e", "#ff8832", "#f44444"
+]
+
+var severity_hint := ""
 var severity : String = "" setget set_severity
+onready var icon : TextureRect = $SeverityTexture
 
 func set_severity(new_severity : String):
 	severity = new_severity
-	$Label.text = ("%s severity" % severity ).capitalize()
-	var color = Color.white
+	var color_string = ""
 	match severity:
 		"low":
-			color = Color("#89d1f1")
+			icon.texture = severity_textures[0]
+			color_string = severity_colors[0]
 		"medium":
-			color = Color("#fffd7e")
+			icon.texture = severity_textures[1]
+			color_string = severity_colors[1]
 		"high":
-			color = Color("#ff8832")
+			icon.texture = severity_textures[2]
+			color_string = severity_colors[2]
 		"critical":
-			color = Color("#f44444")
-			
-	$SeverityTexture.modulate = color
-	$Label.set("custom_colors/font_color", color)
+			icon.texture = severity_textures[3]
+			color_string = severity_colors[3]
+	
+	$SeverityTexture.modulate = Color(color_string)
+	severity_hint = "[color=%s]" % color_string + ("%s severity" % severity ).capitalize() + "[/color]"
+
+
+func _on_SeverityTexture_mouse_entered():
+	_g.emit_signal("tooltip", severity_hint)
+
+
+func _on_SeverityTexture_mouse_exited():
+	_g.emit_signal("tooltip_hide")
