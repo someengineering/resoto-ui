6f98e21a0789d66d3b5cd6721ee10a0d92fae6bc
azagaya <46932830+azagaya@users.noreply.github.com>
2023-03-22 09:34:53 -0300

    Add max tooltip size to avoid tooltips longer than the window (#253)

--- src/components/shared/richtext_uniwidth_resize.gd
+++ src/components/shared/richtext_uniwidth_resize.gd
@@ -10,6 +10,7 @@ func adjust_size() -> void:
 	var new_rect_min_size_x := 0.0
 	for line in text.split("\n"):
 		new_rect_min_size_x = max(new_rect_min_size_x, get_font("normal_font").get_string_size(line).x) + 12.0
-	rect_min_size.x = new_rect_min_size_x
-	rect_size.x = new_rect_min_size_x
+	var new_x_size := min(new_rect_min_size_x, OS.window_size.x - 12.0 )
+	rect_min_size.x = new_x_size
+	rect_size.x = new_x_size
 	get_parent().rect_size = Vector2.ONE
